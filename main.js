d2075afbbc47a87df487b6f8448fc4f058067ce6
G1ovann16 <glandaburo92@gmail.com>
2020-03-14 16:19:06 +0100

    modify background

--- main.js
+++ main.js
@@ -1,24 +1,32 @@
 const urlImagen = 'https://image.tmdb.org/t/p/w200';
 const button = document.getElementById("btn");
-
+let count=5;
+function changeBackground(){
+let num = Math.ceil(Math.random()*count);
+console.log(num);
+document.body.background = 'imag/'+num+'.jpg';
+document.body.style.backgroundRepeat = "repeat";
+}
 
 function buscar(){
-    let count =0;
     let filmsToSearch= "";
-    let k = 0;
     let overView = "";
     let rated=0;
     let temp;
 
     let search = document.getElementById("query").value;
-    if(search.length === 0)
-    document.getElementById('titulo').innerHTML = '';
-    axios({
+    if(search.length <= 0)
+    {document.getElementById('First').innerHTML = '';
+    document.getElementById('Second').innerHTML = '';
+    document.getElementById('Third').innerHTML = '';   
+    changeBackground();
+  }    axios({
         method: 'GET',
         url: `https://api.themoviedb.org/3/search/movie?api_key=cea68b520beecac6718820e4ac576c3a&vi&language=es-ES&query=${search}}`
     }).then(res => {
         let movies = res.data.results;
         for (let i = 0;  i< 6; i++) {
+        if (search.length > 1)  
          if(movies[i].poster_path!=null){
             filmsToSearch += `<img src="${urlImagen}${movies[i].poster_path}">
             <p class="title">${movies[i].title}</p>
@@ -36,6 +44,7 @@ function buscar(){
             }  
                 filmsToSearch='';                
                for (let i = 6;  i< 12; i++) {
+                if (search.length > 1)   
                 if(movies[i].poster_path!=null){
                 filmsToSearch += `<img src="${urlImagen}${movies[i].poster_path}">
                 <p class="title">${movies[i].title}</p>`;
@@ -45,6 +54,7 @@ function buscar(){
                 }
                    filmsToSearch='';                
                    for (let i = 12;  i< 19; i++) {
+                    if (search.length > 1) 
                     if(movies[i].poster_path!=null){
                     filmsToSearch += `<img src="${urlImagen}${movies[i].poster_path}">
                     <p class="title">${movies[i].title}</p>`;
@@ -55,5 +65,3 @@ function buscar(){
                    
                 })
 }
-
-
